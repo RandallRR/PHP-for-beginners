a784eaa8ba28230bd93b6452a2f7a54856419fac
Randall Ramsden <127121977+RandallRR@users.noreply.github.com>
2025-02-13 15:20:54 +0200

    Add project to my GitHub project

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+use Core\Router;
+use Core\Session;
+use Core\ValidationException;
+
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH.'Core/functions.php';
+
+spl_autoload_register(function ($class) {
+    // Core\Database
+    $class =str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
+    require base_path($class . '.php');
+});
+
+require base_path('bootstrap.php');
+
+$router = new Router();
+
+$routes = require base_path('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
+
+    return redirect($router->previousUrl());
+}
+Session::unflash();
